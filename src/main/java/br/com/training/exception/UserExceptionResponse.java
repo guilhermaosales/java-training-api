d2df164b5888190eb5b5fc05bf99700398d18654
Guilherme Sales <guilherme.sales@contabilizei.com.br>
2021-08-20 16:46:50 -0300

    feat: Adjustments on user error handling to provide a better message for API consumers

--- src/main/java/br/com/training/exception/UserExceptionResponse.java
+++ src/main/java/br/com/training/exception/UserExceptionResponse.java
@@ -1,21 +1,20 @@
 package br.com.training.exception;
 
-import org.springframework.validation.FieldError;
-
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class UserExceptionResponse implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private final Instant timestamp;
-    private final String status;
+    private final int status;
     private final String error;
-    private final List<String> message;
+    private final List<Details> message;
     private final String path;
 
-    public UserExceptionResponse(Instant timestamp, String status, String error, List<String> message, String path) {
+    public UserExceptionResponse(Instant timestamp, int status, String error, List<Details> message, String path) {
         this.timestamp = timestamp;
         this.status = status;
         this.error = error;
@@ -27,7 +26,7 @@ public class UserExceptionResponse implements Serializable {
         return timestamp;
     }
 
-    public String getStatus() {
+    public int getStatus() {
         return status;
     }
 
@@ -35,7 +34,7 @@ public class UserExceptionResponse implements Serializable {
         return error;
     }
 
-    public List<String> getMessage() {
+    public List<Details> getMessage() {
         return message;
     }
 
@@ -45,7 +44,7 @@ public class UserExceptionResponse implements Serializable {
 
     @Override
     public String toString() {
-        return "ExceptionResponse{" +
+        return "UserExceptionResponse{" +
                 "timestamp=" + timestamp +
                 ", status='" + status + '\'' +
                 ", error='" + error + '\'' +
@@ -54,4 +53,30 @@ public class UserExceptionResponse implements Serializable {
                 '}';
     }
 
+    public static class Details {
+        private String field;
+        private String message;
+
+        public Details(String field, String message) {
+            this.field = field;
+            this.message = message;
+        }
+
+        public String getField() {
+            return field;
+        }
+
+
+        public String getMessage() {
+            return message;
+        }
+
+        @Override
+        public String toString() {
+            return "Details{" +
+                    "field='" + field + '\'' +
+                    ", message='" + message + '\'' +
+                    '}';
+        }
+    }
 }
