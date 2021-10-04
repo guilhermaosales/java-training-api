9da8a78a8967aeb1217066e22d57d06854b912f5
guilhermaosales <guilhermewrsales@gmail.com>
2021-10-04 02:03:54 -0300

    feat: replace swagger by openapi configuration in API documentation

--- src/main/java/br/com/training/controller/UserController.java
+++ src/main/java/br/com/training/controller/UserController.java
@@ -3,10 +3,11 @@ package br.com.training.controller;
 import br.com.training.controller.dto.request.UserForm;
 import br.com.training.controller.dto.response.UserResponse;
 import br.com.training.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,64 +18,68 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@Api(value = "API REST users")
-@CrossOrigin( origins = "*")
+@Tag(name = "User")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@PostMapping
-	@ApiOperation(value = "Create an user")
-	@ApiResponses(value = {
-			@ApiResponse(code = 201, message = "Returns created user"),
-			@ApiResponse(code = 400, message = "Not found exception"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
+	@PostMapping(produces = "application/json", consumes = "application/json")
+	@Operation(summary = "Create user", responses = {
+			@ApiResponse(description = "Create user success", responseCode = "201",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+			@ApiResponse(description = "User not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
 	})
 	public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserForm user)  {
 		return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
 	}
 
-	@GetMapping (value = "/{cpf}")
-	@ApiOperation(value = "Get an user by cpf")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Returns fetched user"),
-			@ApiResponse(code = 400, message = "Not found exception"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
+	@GetMapping(value = "/{cpf}", produces = "application/json", consumes = "application/json")
+	@Operation(summary = "Get user", responses = {
+			@ApiResponse(description = "Get fetched user", responseCode = "200",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+			@ApiResponse(description = "User not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
 	})
 	public ResponseEntity<UserResponse> getUser(@PathVariable String cpf) {
 		return new ResponseEntity<>(userService.findByCpf(cpf), HttpStatus.OK);
     }
 
-    @GetMapping
-	@ApiOperation(value = "Get all registered users")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Returns fetched users"),
-			@ApiResponse(code = 400, message = "Not found exception"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
+    @GetMapping(produces = "application/json", consumes = "application/json")
+	@Operation(summary = "Get user", responses = {
+			@ApiResponse(description = "Get all users", responseCode = "200",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+			@ApiResponse(description = "User not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
 	})
 	@ResponseStatus(HttpStatus.OK)
 	public List<UserResponse> getAllUsers() {
 		return userService.listAll();
 	}
 
-    @PutMapping(value = "/{cpf}")
-	@ApiOperation(value = "Update an user by cpf")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Returns updated user data"),
-			@ApiResponse(code = 400, message = "Not found exception"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
+    @PutMapping(value = "/{cpf}", produces = "application/json", consumes = "application/json")
+	@Operation(summary = "Update user", responses = {
+			@ApiResponse(description = "Update user", responseCode = "200",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+			@ApiResponse(description = "User not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
 	})
 	public ResponseEntity<UserResponse> updateUser(@RequestBody @Valid UserForm user, @PathVariable String cpf) {
 		return new ResponseEntity<>(userService.update(user, cpf), HttpStatus.OK);
 	}
 
-	@DeleteMapping(value = "/{cpf}")
-	@ApiOperation(value = "Delete an user by cpf")
-	@ApiResponses(value = {
-			@ApiResponse(code = 204, message = "Successfull operation with no content"),
-			@ApiResponse(code = 400, message = "Not found exception"),
-			@ApiResponse(code = 500, message = "Internal Server Error"),
+	@DeleteMapping(value = "/{cpf}", produces = "application/json", consumes = "application/json")
+	@Operation(summary = "Delete user", responses = {
+			@ApiResponse(description = "Delete user", responseCode = "204",
+					content = @Content),
+			@ApiResponse(description = "User not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
 	})
 	public ResponseEntity<Void> deleteUser(@PathVariable String cpf) {
 		userService.deleteByCpf(cpf);
